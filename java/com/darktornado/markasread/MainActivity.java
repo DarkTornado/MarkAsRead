a5918e582a8dfaf1da89a004bb6754a8eaf081bb
Dark Tornado <dt3141592@naver.com>
2023-04-27 18:57:59 +0900

    padding added

--- java/com/darktornado/markasread/MainActivity.java
+++ java/com/darktornado/markasread/MainActivity.java
@@ -3,6 +3,7 @@ package com.darktornado.markasread;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Switch;
 
 public class MainActivity extends Activity {
@@ -15,6 +16,14 @@ public class MainActivity extends Activity {
         Switch on = new Switch(this);
         on.setText("자동 읽음 사용");
         layout.addView(on);
-        setContentView(layout);
+        int pad = dip2px(16);
+        layout.setPadding(pad, pad, pad, pad);
+        ScrollView scroll = new ScrollView(this);
+        scroll.addView(layout);
+        setContentView(scroll);
+    }
+
+    private int dip2px(int dips) {
+        return (int) Math.ceil(dips * getResources().getDisplayMetrics().density);
     }
 }
