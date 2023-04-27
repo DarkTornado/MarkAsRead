bb47033970e78294e2dac476e00eda988283c1b7
Dark Tornado <dt3141592@naver.com>
2023-04-27 18:55:39 +0900

    Main UI

--- java/com/darktornado/markasread/MainActivity.java
+++ java/com/darktornado/markasread/MainActivity.java
@@ -2,11 +2,19 @@ package com.darktornado.markasread;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+        Switch on = new Switch(this);
+        on.setText("자동 읽음 사용");
+        layout.addView(on);
+        setContentView(layout);
     }
 }
